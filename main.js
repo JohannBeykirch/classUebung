c133631c73708ccc8e043485c6c164091f7d0860
Johann Beykirch <beykirchjohann@gmail.com>
2022-06-20 10:07:31 +0200

    Bücher1-4 sind fertig

--- main.js
+++ main.js
@@ -34,5 +34,11 @@ let lager = []
 
 function init(){
     let Buch1 = new Buch (1,"Unterm Rad", "Hermann Hesse", "17.01.2001", "Suhrkamp", 176)
-    let Buch2 = new Buch ()
+    lager.push(Buch1)
+    let Buch2 = new Buch (1, "Gravitation", "Kip S. Thorne, John Wheeler, Charles W. Misner", "24.10.2017", "Princeton Univers. Press", 1328)
+    lager.push(Buch2)
+    let Buch3 = new Buch (1, "Die physikalischen Prinzipien der Quantentheorie", "Werner Heisenberg", "1.10.2008", "S. Hirzel Verlag GmbH", 137)
+    lager.push(Buch3)
+    let Buch4 = new Buch (1, "Über die spezielle und allgemeine Relativitätstheorie", "Albert Einstein", "14.09.2012", "Springer", 124)
+    lager.push(Buch4)
 }
