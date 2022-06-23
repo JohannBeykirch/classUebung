99f939c1d7bcb7a56f5b0c2c8a539a5889467e64
Johann Beykirch <beykirchjohann@gmail.com>
2022-06-23 14:03:59 +0200

    Lager ist sortiert und die function ausDemSortiment() wurde angefangen.

--- main.js
+++ main.js
@@ -33,13 +33,24 @@ class Buch{
 let lager = []
 
 function init(){
-    let Buch1 = new Buch (1,"Unterm Rad", "Hermann Hesse", "17.01.2001", "Suhrkamp", 176)
+    let Buch1 = new Buch (1,"Unterm Rad", "Hesse, Hermann ", "17.01.2001", "Suhrkamp", 176)
     lager.push(Buch1)
-    let Buch2 = new Buch (1, "Gravitation", "Kip S. Thorne, John Wheeler, Charles W. Misner", "24.10.2017", "Princeton Univers. Press", 1328)
+    let Buch2 = new Buch (1, "Gravitation", "Thorne, Kip S.", "24.10.2017", "Princeton Univers. Press", 1328)
     lager.push(Buch2)
-    let Buch3 = new Buch (1, "Die physikalischen Prinzipien der Quantentheorie", "Werner Heisenberg", "1.10.2008", "S. Hirzel Verlag GmbH", 137)
+    let Buch3 = new Buch (1, "Die physikalischen Prinzipien der Quantentheorie", "Heisenberg, Werner", "1.10.2008", "S. Hirzel Verlag GmbH", 137)
     lager.push(Buch3)
-    let Buch4 = new Buch (1, "Über die spezielle und allgemeine Relativitätstheorie", "Albert Einstein", "14.09.2012", "Springer", 124)
+    let Buch4 = new Buch (1, "Über die spezielle und allgemeine Relativitätstheorie", "Einstein, Albert", "14.09.2012", "Springer", 124)
     lager.push(Buch4)
-
+    lager.sort(this.Autor)
 }
+init()
+
+console.log(lager.length)
+console.log(lager)
+function ausDemSortiment(buchtitel){
+        for(let i = lager.length; i === 0; i--) {
+            lager.pop()
+            console.log(lager)
+        }
+}
+console.log(lager)
